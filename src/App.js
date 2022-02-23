6e107e152bdba0dd1c3b1a46ee08de5708b1aae2
William Nollert <will.nollert@gmail.com>
2022-02-23 08:28:25 -0600

    feat: added Form and Post component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 import memories from "./images/memories.png";
-const app = () => {
+
+const App = () => {
   return (
     <Container maxwidth="lg">
       <AppBar position="static" color="inherit">
@@ -11,8 +14,25 @@ const app = () => {
         </Typography>
         <img src={memories} alt="memories" height="60" />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
 
-export default app;
+export default App;
