869c33b319123e2c88b55183e5a5a11211719968
William Nollert <will.nollert@gmail.com>
2022-02-23 07:55:05 -0600

    feat: added placeholder file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
+import memories from "./images/memories.png";
 const app = () => {
-  return <div>app</div>;
+  return (
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Race Report
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+    </Container>
+  );
 };
 
 export default app;
